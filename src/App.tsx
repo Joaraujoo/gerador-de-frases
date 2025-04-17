ec77ec4020ed1634c592edf3d3d9f42d608ecce5
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-04-17 16:02:24 -0300

    css

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,21 @@ export default function App() {
   return(
     <div className='container'>
 
-      
+      <img 
+        src={logoImg} 
+        alt="Logo dev frases" 
+        className='logo'
+      />
+
+      <h2 className='title'>Categorias</h2>
+      <section className='category-area'>
+        <button className='category-button'>Motivação</button>
+        <button className='category-button'>Bem estar</button>
+      </section>
+
+      <button className='button-frase'>Gerar Frase</button>
+
+      <p className='texto-frase'>Alguma frase vai vir aqui</p>
 
     </div>
   )
