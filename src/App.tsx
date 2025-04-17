f27a16229d6ea9e9fd95c84136fba8133b53bf56
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-04-17 15:54:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 
+import logoImg from "./assets/logo.png"
+
+
 export default function App() {
 
-  
+
 
   return(
-    <div>
+    <div className='container'>
+
+      
 
     </div>
   )
